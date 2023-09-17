515ff482b2259e98ef469c977c00a678dd1ad32b
Raze Viana <57551859+RazeViana@users.noreply.github.com>
2023-09-17 17:53:53 +0200

    Added Reload ability

---
+++ commands/utility/reload.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	category: 'utility',
+	data: new SlashCommandBuilder()
+		.setName('reload')
+		.setDescription('Reloads a command.')
+		.addStringOption(option =>
+			option.setName('command')
+				.setDescription('The command to reload.')
+				.setRequired(true)),
+	async execute(interaction) {
+		const commandName = interaction.options.getString('command', true).toLowerCase();
+		const command = interaction.client.commands.get(commandName);
+
+		if (!command) {
+			return interaction.reply({ content: `There is no command with name \`${commandName}\``, ephemeral: true });
+		}
+
+		try {
+			delete require.cache[require.resolve(`../${command.category}/${command.data.name}.js`)];
+		}
+		catch (error) {
+			console.error(error);
+			await interaction.reply({ content: 'Command missing category, sort it out mate', ephemeral: true });
+		}
+
+		try {
+			interaction.client.commands.delete(command.data.name);
+			const newCommand = require(`../${command.category}/${command.data.name}.js`);
+			interaction.client.commands.set(newCommand.data.name, newCommand);
+			await interaction.reply(`Command \`${newCommand.data.name}\` was reloaded!`);
+		}
+		catch (error) {
+			console.error(error);
+			await interaction.reply({ content:`There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``, ephemeral: true });
+		}
+	},
+};
