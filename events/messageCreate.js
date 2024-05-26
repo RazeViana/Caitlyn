e951c03b5d5af850e37f755242905bc7cfe9d865
Raze Viana <razeviana@hotmail.com>
2024-05-26 15:01:08 +0200

    Updated message send to catch error

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,71 +1,65 @@
-const { Events, EmbedBuilder } = require('discord.js');
-const axios = require('axios');
+const { Events, EmbedBuilder } = require("discord.js");
+const axios = require("axios");
 
 module.exports = {
-	name: Events.MessageCreate,
-	async execute(message) {
-		if (message.author.bot) return;
+  name: Events.MessageCreate,
+  async execute(message) {
+    if (message.author.bot) return;
 
-		// Changes twitter.com links with the vx at the back
-		const twitterBaseURL = 'https://x.com';
-		const vxtwitterAPIBaseURL = 'https://api.vxtwitter.com';
+    // Changes twitter.com links with the vx at the back
+    const twitterBaseURL = "https://x.com";
+    const vxtwitterAPIBaseURL = "https://api.vxtwitter.com";
 
-		if (message.content.startsWith(twitterBaseURL)) {
-			try {
-				const vxTwitterAPI = await axios.get(message.content.replace(twitterBaseURL, vxtwitterAPIBaseURL));
-				// Checks if twitter post contains a video
-				if (vxTwitterAPI.data.mediaURLs.length === 1) {
-					if (vxTwitterAPI.data.mediaURLs[0].includes('video')) {
-						await message.delete();
-						message.channel.send(`${vxTwitterAPI.data.mediaURLs[0]}`);
-						message.channel.send(`${vxTwitterAPI.data.text.replace(/https?:\/\/\S+/g, '')}`);
-					}
-					else if (vxTwitterAPI.data.mediaURLs[0].includes('img')) {
-						const twitterImageEmbed = new EmbedBuilder()
-							.setAuthor({ name: `@${vxTwitterAPI.data.user_name}`, url: vxTwitterAPI.data.tweetURL })
-							.setTitle(vxTwitterAPI.data.user_screen_name)
-							.setURL(vxTwitterAPI.data.tweetURL)
-							.setThumbnail('https://pbs.twimg.com/profile_images/1683899100922511378/5lY42eHs_400x400.jpg')
-							.setColor(4506111)
-							.setDescription(vxTwitterAPI.data.text.replace(/https?:\/\/\S+/g, ''))
-							.setImage(vxTwitterAPI.data.mediaURLs[0])
-							.setFooter({ text: message.author.tag, iconURL: message.author.avatarURL() });
+    if (message.content.startsWith(twitterBaseURL)) {
+      try {
+        const vxTwitterAPI = await axios.get(
+          message.content.replace(twitterBaseURL, vxtwitterAPIBaseURL)
+        );
+        // Checks if twitter post contains a video
+        if (vxTwitterAPI.data.mediaURLs.length === 1) {
+          if (vxTwitterAPI.data.mediaURLs[0].includes("video")) {
+            // Deletes the original message
+            await message.delete();
+            // Sends the video link from the api
+            if (vxTwitterAPI.data.mediaURLs[0]) {
+              await message.channel.send(vxTwitterAPI.data.mediaURLs[0]);
+            }
+            const textContent = vxTwitterAPI.data.text
+              .replace(/https?:\/\/\S+/g, "")
+              .trim();
+            if (textContent) {
+              await message.channel.send(textContent);
+            }
+          }
+          // Checks if twitter post contains an image
+          else if (vxTwitterAPI.data.mediaURLs[0].includes("img")) {
+            const twitterImageEmbed = new EmbedBuilder()
+              .setAuthor({
+                name: `@${vxTwitterAPI.data.user_name}`,
+                url: vxTwitterAPI.data.tweetURL,
+              })
+              .setTitle(vxTwitterAPI.data.user_screen_name)
+              .setURL(vxTwitterAPI.data.tweetURL)
+              .setThumbnail(
+                "https://pbs.twimg.com/profile_images/1683899100922511378/5lY42eHs_400x400.jpg"
+              )
+              .setColor(4506111)
+              .setDescription(
+                vxTwitterAPI.data.text.replace(/https?:\/\/\S+/g, "")
+              )
+              .setImage(vxTwitterAPI.data.mediaURLs[0])
+              .setFooter({
+                text: message.author.tag,
+                iconURL: message.author.avatarURL(),
+              });
 
-						await message.delete();
-						message.channel.send({ embeds: [twitterImageEmbed] });
-					}
-				}
-				// 	await message.delete();
-				// 	message.channel.send({ embeds: [twitterImageEmbed] });
-				// }
-
-				// // Multiple Image Embed
-				// if (vxTwitterAPI.data.mediaURLs.length > 1) {
-				// 	const twitterEmbeds = new EmbedBuilder()
-				// 		.setAuthor({ name: `@ ${vxTwitterAPI.data.user_name}`, url: vxTwitterAPI.data.tweetURL })
-				// 		.setTitle(vxTwitterAPI.data.user_screen_name)
-				// 		.setURL(vxTwitterAPI.data.tweetURL)
-				// 		.setThumbnail('https://pbs.twimg.com/profile_images/1683899100922511378/5lY42eHs_400x400.jpg')
-				// 		.setColor(4506111)
-				// 		.setDescription(vxTwitterAPI.data.text)
-				// 		.setImage(vxTwitterAPI.data.mediaURLs[0])
-				// 		.setFooter({ text: message.author.tag, iconURL: message.author.avatarURL() });
-
-				// 	const twitterEmbeds1 = new EmbedBuilder()
-				// 		.setURL(vxTwitterAPI.data.tweetURL)
-				// 		.setImage(vxTwitterAPI.data.mediaURLs[1]);
-
-				// 	const twitterEmbeds2 = new EmbedBuilder()
-				// 		.setURL(vxTwitterAPI.data.tweetURL)
-				// 		.setImage(vxTwitterAPI.data.mediaURLs[1]);
-
-				// 	await message.delete();
-				// 	message.channel.send({ embeds: [twitterEmbeds, twitterEmbeds1, twitterEmbeds2] });
-				// }
-			}
-			catch (error) {
-				// twitter link not real
-			}
-		}
-	},
+            await message.delete();
+            await message.channel.send({ embeds: [twitterImageEmbed] });
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching data from vxtwitter API:", error);
+      }
+    }
+  },
 };
