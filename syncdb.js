d8e02f6a130215a55d6ec5474bac08f0ccd8e44d
Raze Viana <RazeViana@hotmail.com>
2025-04-11 23:43:01 +0200

    Initial Refactor

--- syncdb.js
+++
@@ -1,7 +0,0 @@
-const Birthdays = require('./models/birthdays');
-
-// Force = Wipes db and starts fresh
-// Birthdays.sync({ force: true });
-
-// Alter = Adds new tables to db
-// Birthdays.sync({ alter: true });
